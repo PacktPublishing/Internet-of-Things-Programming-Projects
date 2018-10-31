3eb6f06f5118446593545aeff33792223fac1e60
Sigma Rockets <sales@sigmarockets.com>
2018-10-31 02:47:55 -0400

    minor changes

--- Chapter 18/code/bluepy-example.py
+++
@@ -1,8 +0,0 @@
-from bluepy.btle import Scanner
-scanner = Scanner()
-devices = scanner.scan(10.0)
-
-for dev in devices:
-    print "Device %s (%s), RSSI=%d dB" % (dev.addr, dev.addrType, dev.rssi)
-    for (adtype, desc, value) in dev.getScanData():
-        print "  %s = %s" % (desc, value)
